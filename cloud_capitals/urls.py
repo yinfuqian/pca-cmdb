229d7d1a3e29b3e94b770b757f3510f1cfb8e18c
Frank Chen <frankchen@wezhuiyi.com>
2023-01-06 18:51:28 +0800

    Revert "new"
    This reverts commit c4561ead4e14902b662309e63d05e3e8ed793c17.

--- cloud_capitals/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include
-from django.urls import re_path
-from .views import *
-
-urlpatterns = [
-    # re_path(r'^index', IndexView.as_view(), name='index'),
-    re_path(r'/$', CloudView.as_view(), name='cloud'),
-    re_path(r'/cloud_create', CloudCreateView.as_view(), name='cloud_create')
-]
--- dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# 云机器
-from django.conf.urls import include
-from django.urls import re_path
-from .views import *
-urlpatterns = [
-    # re_path(r'^index', IndexView.as_view(), name='index'),
-    re_path(r'^cloud_capitals', include('cloud_capitals.urls'))
-]
